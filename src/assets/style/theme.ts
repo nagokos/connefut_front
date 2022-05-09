6734766a2ebf97f0014edb8ef63f7267207632b1
Naohiro Kosuda <kosnago0428@gmail.com>
2022-05-10 07:46:14 +0900

    add theme

--- src/assets/style/theme.ts
+++ src/assets/style/theme.ts
@@ -2,10 +2,11 @@ import { extendTheme } from '@chakra-ui/react';
 
 export const theme = extendTheme({
   colors: {
-    primary: {
-      main: '#009688',
-      light: '#f0f5f4',
-    },
+    primary: '#009688',
+    light: '#f0f5f4',
+  },
+  shadows: {
+    menu: '0 3px 12px -1px #04253f40;',
   },
   components: {
     Button: {
