85cbf0cffbba93f2e0fb64b345e6c2f3835a461b
Naohiro Kosuda <kosnago0428@gmail.com>
2022-05-15 17:08:38 +0900

    UIの修正

--- src/components/uiGroup/Header/Title.tsx
+++ src/components/uiGroup/Header/Title.tsx
@@ -1,13 +1,27 @@
-import { FC, memo } from 'react';
-import { Image, Text } from '@chakra-ui/react';
+import { FC, memo, startTransition } from 'react';
+import { Box, Image, Text } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
 
 export const Title: FC = memo(() => {
+  const navigate = useNavigate();
+
   return (
     <>
-      <Image src="/src/assets/img/logo.png" alt="app logo" w={7} mr={1.5} />
-      <Text fontFamily="Nunito" fontWeight="bold" fontSize={24}>
-        Connefut
-      </Text>
+      <Box
+        display="flex"
+        alignItems="center"
+        cursor="pointer"
+        onClick={() =>
+          startTransition(() => {
+            navigate('/');
+          })
+        }
+      >
+        <Image src="/src/assets/img/logo.png" alt="app logo" w={7} mr={1.5} />
+        <Text fontFamily="Nunito" fontWeight="bold" fontSize={25}>
+          Connefut
+        </Text>
+      </Box>
     </>
   );
 });
