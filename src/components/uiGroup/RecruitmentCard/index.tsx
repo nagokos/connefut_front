b8778ed1131ae91aacf6bba7a309e1c64e903b43
Naohiro Kosuda <kosnago0428@gmail.com>
2022-05-15 17:16:19 +0900

    募集を表示できるように

--- src/components/uiGroup/RecruitmentCard/index.tsx
+++ src/components/uiGroup/RecruitmentCard/index.tsx
@@ -1,43 +1,54 @@
 import { FC, memo } from 'react';
 import { Box, Stack } from '@chakra-ui/react';
 
-import { CardRecruitment } from '../../../type/type';
 import { Title } from './Title';
 import { Header } from './Header';
 import { Area } from './Area';
 import { Competition } from './Competition';
 import { Closing } from './Closing';
 import { Author } from './Author';
+import { useGetRecruitmentsQuery } from '../../../generated/graphql';
 
-type Props = {
-  recruitment: CardRecruitment;
-};
-
-export const RecruitmentCard: FC<Props> = memo((props) => {
-  const { recruitment } = props;
+export const RecruitmentCard: FC = memo(() => {
+  const [data] = useGetRecruitmentsQuery({
+    variables: {
+      paginationInput: {
+        first: 10,
+        last: null,
+        after: null,
+        before: null,
+        options: {},
+      },
+    },
+  });
 
   return (
-    <Box
-      cursor="pointer"
-      bg="white"
-      boxShadow="0px 3px 4px rgb(236 239 241)"
-      _hover={{ boxShadow: '0px 10px 10px rgb(236 239 241)' }}
-      rounded="xl"
-      transitionDuration="300ms"
-      overflow="hidden"
-    >
-      <Header recruitment={recruitment} />
-      <Box px={4} py={4}>
-        <Title recruitment={recruitment} />
-        <Stack spacing={4} mt={7}>
-          <Area recruitment={recruitment} />
-          <Competition recruitment={recruitment} />
-          <Closing recruitment={recruitment} />
-        </Stack>
-        <Box mt={5}>
-          <Author recruitment={recruitment} />
+    <>
+      {data.data?.getRecruitments.edges.map((edge) => (
+        <Box
+          key={edge.cursor}
+          cursor="pointer"
+          bg="white"
+          boxShadow="0px 3px 4px rgb(236 239 241)"
+          _hover={{ boxShadow: '0px 10px 10px rgb(236 239 241)' }}
+          rounded="xl"
+          transitionDuration="300ms"
+          overflow="hidden"
+        >
+          <Header recruitment={edge.node} />
+          <Box px={4} py={4}>
+            <Title recruitment={edge.node} />
+            <Stack spacing={4} mt={7}>
+              <Area recruitment={edge.node} />
+              <Competition recruitment={edge.node} />
+              <Closing recruitment={edge.node} />
+            </Stack>
+            <Box mt={5}>
+              <Author recruitment={edge.node} />
+            </Box>
+          </Box>
         </Box>
-      </Box>
-    </Box>
+      ))}
+    </>
   );
 });
