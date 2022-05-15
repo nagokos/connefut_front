c5742112165c300a32d5d4a243e49114e6ff03b6
Naohiro Kosuda <kosnago0428@gmail.com>
2022-05-15 17:10:47 +0900

    add component SubmitButton.tsx

---
+++ src/components/uiParts/SubmitButton.tsx
@@ -0,0 +1,23 @@
+import { Button } from '@chakra-ui/react';
+import { FC, memo } from 'react';
+
+type Props = {
+  isSubmitting: boolean;
+};
+
+export const SubmitButton: FC<Props> = memo((props) => {
+  const { isSubmitting } = props;
+
+  return (
+    <Button
+      fontSize={13}
+      isFullWidth
+      isLoading={isSubmitting}
+      bg="primary.main"
+      color="white"
+      type="submit"
+    >
+      登録する
+    </Button>
+  );
+});
