9d79bf26bae5ed306bff443869b0b08a929d81e2
Naohiro Kosuda <kosnago0428@gmail.com>
2022-05-15 17:11:23 +0900

    add components

--- src/components/uiGroup/index.ts
+++ src/components/uiGroup/index.ts
@@ -1,3 +1,5 @@
 export { Header } from './Header/index';
 export { HomeSearch } from './HomeSearch/index';
 export { RecruitmentCard } from './RecruitmentCard/index';
+export { SignUpEmailModal } from './SignUpEmailModal';
+export { LoginEmailModal } from './LoginEmailModal';
