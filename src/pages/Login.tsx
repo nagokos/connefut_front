82338718ad22cc593c52972c157ab211b15a2ef0
Naohiro Kosuda <kosnago0428@gmail.com>
2022-05-15 17:06:44 +0900

    UIの修正

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,20 @@
-import { Box, Stack, Image, Divider, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Stack,
+  Image,
+  Divider,
+  Text,
+  useDisclosure,
+} from '@chakra-ui/react';
 import { FC, memo } from 'react';
 import { MdEmail } from 'react-icons/md';
 import { useNavigate } from 'react-router-dom';
+import { LoginEmailModal } from '../components/uiGroup';
 import { AppInButton, ArrowLeftButton } from '../components/uiParts';
 
 export const Login: FC = memo(() => {
   const navigate = useNavigate();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <>
@@ -16,40 +25,31 @@ export const Login: FC = memo(() => {
           textAlign="center"
           fontWeight="bold"
           fontSize={28}
-          color="blackAlpha.700"
+          color="blackAlpha.800"
         >
           ログイン
         </Text>
         <Stack spacing={6}>
-          <Box>
-            <AppInButton
-              clickEvent={() => navigate('/')}
-              icon={<MdEmail color="#f42121" size={18} />}
-              title="メールアドレスでログイン"
-              color="#757575"
-            />
-          </Box>
-          <Box>
-            <AppInButton
-              clickEvent={() => navigate('/')}
-              icon={<Image src="/src/assets/img/google.png" w={4} />}
-              title="Googleでログイン"
-              color="#757575"
-            />
-          </Box>
-          <Box>
-            <AppInButton
-              clickEvent={() => navigate('/')}
-              icon={<Image src="/src/assets/img/line.png" w={4} />}
-              title="LINEでログイン"
-              color="#757575"
-            />
-          </Box>
+          <AppInButton
+            clickEvent={onOpen}
+            icon={<MdEmail color="#f42121" size={18} />}
+            title="メールアドレスでログイン"
+          />
+          <AppInButton
+            clickEvent={() => navigate('/')}
+            icon={<Image src="/src/assets/img/google.png" w={4} />}
+            title="Googleでログイン"
+          />
+          <AppInButton
+            clickEvent={() => navigate('/')}
+            icon={<Image src="/src/assets/img/line.png" w={4} />}
+            title="LINEでログイン"
+          />
           <Box pt={3}>
             <Divider border="1px solid #f0f5f4" borderColor="primary.light" />
           </Box>
           <Box textAlign="center">
-            <Text color="#757575" fontSize={13} fontWeight="bold">
+            <Text color="blackAlpha.800" fontSize={13} fontWeight="bold">
               アカウントをお持ちでない方
             </Text>
             <Box mt={5}>
@@ -62,6 +62,7 @@ export const Login: FC = memo(() => {
           </Box>
         </Stack>
       </Box>
+      <LoginEmailModal isOpen={isOpen} onClose={onClose} />
     </>
   );
 });
