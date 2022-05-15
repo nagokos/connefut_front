b67affe3e43dac90b2348c5c2b8356f0d94df305
Naohiro Kosuda <kosnago0428@gmail.com>
2022-05-15 17:06:58 +0900

    UIの修正

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -25,40 +25,31 @@ export const SignUp: FC = memo(() => {
           textAlign="center"
           fontWeight="bold"
           fontSize={28}
-          color="blackAlpha.700"
+          color="blackAlpha.800"
         >
           新規登録
         </Text>
         <Stack spacing={6}>
-          <Box>
-            <AppInButton
-              clickEvent={onOpen}
-              icon={<MdEmail color="#f42121" size={18} />}
-              title="メールアドレスで登録"
-              color="#757575"
-            />
-          </Box>
-          <Box>
-            <AppInButton
-              clickEvent={() => navigate('/')}
-              icon={<Image src="/src/assets/img/google.png" w={4} />}
-              title="Googleで登録"
-              color="#757575"
-            />
-          </Box>
-          <Box>
-            <AppInButton
-              clickEvent={() => navigate('/')}
-              icon={<Image src="/src/assets/img/line.png" w={4} />}
-              title="LINEで登録"
-              color="#757575"
-            />
-          </Box>
+          <AppInButton
+            clickEvent={onOpen}
+            icon={<MdEmail color="#f42121" size={18} />}
+            title="メールアドレスで登録"
+          />
+          <AppInButton
+            clickEvent={() => navigate('/')}
+            icon={<Image src="/src/assets/img/google.png" w={4} />}
+            title="Googleで登録"
+          />
+          <AppInButton
+            clickEvent={() => navigate('/')}
+            icon={<Image src="/src/assets/img/line.png" w={4} />}
+            title="LINEで登録"
+          />
           <Box pt={3}>
             <Divider border="1px solid #f0f5f4" borderColor="primary.light" />
           </Box>
           <Box textAlign="center">
-            <Text color="#757575" fontSize={13} fontWeight="bold">
+            <Text color="blackAlpha.800" fontSize={13} fontWeight="bold">
               アカウントをお持ちの方
             </Text>
             <Box mt={5}>
