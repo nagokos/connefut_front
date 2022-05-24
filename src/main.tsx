36b1fa4653d2657f66ffddfe6d76c2f23c8c8c6e
Naohiro Kosuda <kosnago0428@gmail.com>
2022-05-24 23:34:22 +0900

    add recoil

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
-import { Box, ChakraProvider, Image } from '@chakra-ui/react';
 import { Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Box, ChakraProvider, Image } from '@chakra-ui/react';
+import { RecoilRoot } from 'recoil';
 import { Provider } from 'urql';
 import { App } from './App';
 import { theme } from './assets/theme/theme';
@@ -12,25 +13,27 @@ const container = document.getElementById('root');
 if (container) {
   const root = createRoot(container);
   root.render(
-    <BrowserRouter>
-      <Provider value={client}>
-        <ChakraProvider theme={theme}>
-          <Suspense
-            fallback={
-              <Box
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-                minH="100vh"
-              >
-                <Image src="/src/assets/img/logo.png" w={12} />
-              </Box>
-            }
-          >
-            <App />
-          </Suspense>
-        </ChakraProvider>
-      </Provider>
-    </BrowserRouter>
+    <Provider value={client}>
+      <ChakraProvider theme={theme}>
+        <BrowserRouter>
+          <RecoilRoot>
+            <Suspense
+              fallback={
+                <Box
+                  display="flex"
+                  justifyContent="center"
+                  alignItems="center"
+                  minH="100vh"
+                >
+                  <Image src="/src/assets/img/logo.png" w={12} />
+                </Box>
+              }
+            >
+              <App />
+            </Suspense>
+          </RecoilRoot>
+        </BrowserRouter>
+      </ChakraProvider>
+    </Provider>
   );
 }
