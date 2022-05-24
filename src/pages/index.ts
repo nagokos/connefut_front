1a88c7f9972dc9aa12a6270701111c904261a1e3
Naohiro Kosuda <kosnago0428@gmail.com>
2022-05-24 22:14:55 +0900

    add pages

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,5 @@
 export { Home } from './Home';
 export { SignUp } from './SignUp';
 export { Login } from './Login';
+export { RecruitmentNew } from './RecruitmentNew';
+export { RecruitmentDetails } from './RecruitmentDetails';
