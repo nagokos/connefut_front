eac319a51ae5d690c6ce87b2468d43d9c9ff7afb
Naohiro Kosuda <kosnago0428@gmail.com>
2022-05-25 00:00:23 +0900

    remove recoil

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Box, ChakraProvider, Image } from '@chakra-ui/react';
-import { RecoilRoot } from 'recoil';
 import { Provider } from 'urql';
 import { App } from './App';
 import { theme } from './assets/theme/theme';
@@ -16,22 +15,20 @@ if (container) {
     <Provider value={client}>
       <ChakraProvider theme={theme}>
         <BrowserRouter>
-          <RecoilRoot>
-            <Suspense
-              fallback={
-                <Box
-                  display="flex"
-                  justifyContent="center"
-                  alignItems="center"
-                  minH="100vh"
-                >
-                  <Image src="/src/assets/img/logo.png" w={12} />
-                </Box>
-              }
-            >
-              <App />
-            </Suspense>
-          </RecoilRoot>
+          <Suspense
+            fallback={
+              <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                minH="100vh"
+              >
+                <Image src="/src/assets/img/logo.png" w={12} />
+              </Box>
+            }
+          >
+            <App />
+          </Suspense>
         </BrowserRouter>
       </ChakraProvider>
     </Provider>
