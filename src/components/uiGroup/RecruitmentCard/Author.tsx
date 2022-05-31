4cc3ddcbaf44ee7e570423c23a73c073dbc13eba
Naohiro Kosuda <kosnago0428@gmail.com>
2022-05-31 19:22:30 +0900

    UIの修正

--- src/components/uiGroup/RecruitmentCard/Author.tsx
+++ src/components/uiGroup/RecruitmentCard/Author.tsx
@@ -1,4 +1,6 @@
 import { Avatar, Box, Stack, Text } from '@chakra-ui/react';
+import { formatDistanceToNow } from 'date-fns';
+import { ja } from 'date-fns/locale';
 import { FC, memo } from 'react';
 import { CardRecruitment } from '../../../type/type';
 
@@ -9,12 +11,24 @@ type Props = {
 export const Author: FC<Props> = memo((props) => {
   const { recruitment } = props;
 
+  const distanceToNow = () => {
+    return formatDistanceToNow(new Date(String(recruitment.published_at)), {
+      locale: ja,
+    }).replace('約', '');
+  };
+
   return (
     <Box display="flex" alignItems="center">
       <Avatar mr={2} w={10} h={10} src={recruitment.user.avatar} />
       <Stack direction="column" spacing="0" fontSize={12}>
-        <Text fontWeight="600">Daisuke</Text>
-        <Text color="gray.500">10日前に掲載</Text>
+        <Text fontWeight="bold">{recruitment.user.name}</Text>
+        <Text
+          color="blackAlpha.500"
+          fontSize={10}
+          fontFamily="ヒラギノ角ゴシック"
+        >
+          {distanceToNow()}前に公開
+        </Text>
       </Stack>
     </Box>
   );
