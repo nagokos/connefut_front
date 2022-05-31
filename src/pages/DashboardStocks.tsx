9fa3a01377f85e6e8c06c054f04e748ce973198c
Naohiro Kosuda <kosnago0428@gmail.com>
2022-05-31 23:30:42 +0900

    add page DashboardStocks.tsx

---
+++ src/pages/DashboardStocks.tsx
@@ -0,0 +1,10 @@
+import { Box } from '@chakra-ui/react';
+import { FC, memo } from 'react';
+
+export const DashboardStocks: FC = memo(() => {
+  return (
+    <Box>
+      <Box>Stocks</Box>
+    </Box>
+  );
+});
