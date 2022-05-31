60dfa403524032525a19a8696c4a8d9917513438
Naohiro Kosuda <kosnago0428@gmail.com>
2022-06-01 04:56:26 +0900

    クリックで/dashboardへ遷移

--- src/components/uiGroup/DashboardButtons/Recruitments.tsx
+++ src/components/uiGroup/DashboardButtons/Recruitments.tsx
@@ -1,10 +1,21 @@
 import { Box } from '@chakra-ui/react';
 import { FC, memo } from 'react';
 import { MdOutlineArticle } from 'react-icons/md';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export const Recruitments: FC = memo(() => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
   return (
-    <Box display="flex" alignItems="center" color="black" cursor="pointer">
+    <Box
+      display="flex"
+      alignItems="center"
+      cursor="pointer"
+      color={!location.pathname.includes('stocks') ? 'black' : 'blackAlpha.500'}
+      onClick={() => navigate('/dashboard')}
+      _hover={{ color: 'black' }}
+    >
       <Box fontSize={20} color="primary.main">
         <MdOutlineArticle />
       </Box>
