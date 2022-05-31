d83754b8e8270b4e14d6a8cc26d333e9d1c2754c
Naohiro Kosuda <kosnago0428@gmail.com>
2022-06-01 04:54:51 +0900

    募集をストックできるように

--- src/components/uiGroup/RecruitmentDetailActions/Stock.tsx
+++ src/components/uiGroup/RecruitmentDetailActions/Stock.tsx
@@ -1,8 +1,44 @@
 import { Button, Text } from '@chakra-ui/react';
 import { FC, memo } from 'react';
-import { MdOutlineBookmarkBorder } from 'react-icons/md';
+import { MdBookmark, MdOutlineBookmarkBorder } from 'react-icons/md';
+import { useParams } from 'react-router-dom';
+import {
+  useCheckStockedQuery,
+  useCreateStockMutation,
+  useDeleteStockMutation,
+} from '../../../generated/graphql';
 
 export const Stock: FC = memo(() => {
+  const { recruitmentId } = useParams();
+
+  const [data, executeQuery] = useCheckStockedQuery({
+    variables: {
+      recruitmentId: String(recruitmentId),
+    },
+  });
+  const [_c, createStock] = useCreateStockMutation();
+  const [_d, deleteStock] = useDeleteStockMutation();
+
+  const isStocked = !!data.data?.checkStocked;
+
+  const createOrDeleteStock = async () => {
+    const variables = { recruitmentId: String(recruitmentId) };
+
+    executeQuery({
+      requestPolicy: 'network-only',
+    });
+
+    if (isStocked) {
+      await deleteStock(variables);
+    } else {
+      await createStock(variables);
+    }
+
+    executeQuery({
+      requestPolicy: 'network-only',
+    });
+  };
+
   return (
     <>
       <Text fontSize={13.5} letterSpacing={1} fontFamily="Noto Sans JP">
@@ -14,6 +50,17 @@ export const Stock: FC = memo(() => {
         w="100%"
         color="blackAlpha.700"
         fontSize={13}
+        boxShadow="0 0 0 1px rgb(0 0 0 / 2%), 0 5px 8px 0 rgb(0 0 0 / 10%)"
+        leftIcon={
+          <>
+            {isStocked ? (
+              <MdBookmark size={16} color="RGBA(0, 0, 0, 0.40)" />
+            ) : (
+              <MdOutlineBookmarkBorder size={16} color="RGBA(0, 0, 0, 0.48)" />
+            )}
+          </>
+        }
+        onClick={createOrDeleteStock}
         _hover={{
           bg: 'blackAlpha.50',
         }}
@@ -23,12 +70,8 @@ export const Stock: FC = memo(() => {
         _focus={{
           boxShadow: '0 0 0 1px rgb(0 0 0 / 2%), 0 5px 8px 0 rgb(0 0 0 / 10%)',
         }}
-        boxShadow="0 0 0 1px rgb(0 0 0 / 2%), 0 5px 8px 0 rgb(0 0 0 / 10%)"
-        leftIcon={
-          <MdOutlineBookmarkBorder size={16} color="RGBA(0, 0, 0, 0.48)" />
-        }
       >
-        ストックする
+        {isStocked ? 'ストック済み' : 'ストックする'}
       </Button>
     </>
   );
