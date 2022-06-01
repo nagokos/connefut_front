ea1e44ebafe749b80a029c0cb593d91b765b6f9d
Naohiro Kosuda <kosnago0428@gmail.com>
2022-06-01 23:51:23 +0900

    add onClick

--- src/components/uiGroup/DashboardRecruitment/Title.tsx
+++ src/components/uiGroup/DashboardRecruitment/Title.tsx
@@ -1,15 +1,25 @@
 import { Box } from '@chakra-ui/react';
 import { FC, memo } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 type Props = {
+  id: string;
   title: string;
 };
 
 export const Title: FC<Props> = memo((props) => {
-  const { title } = props;
+  const { id, title } = props;
+
+  const navigate = useNavigate();
 
   return (
-    <Box cursor="pointer" mt={2} fontSize={16} fontWeight="bold">
+    <Box
+      cursor="pointer"
+      mt={2}
+      fontSize={16}
+      fontWeight="bold"
+      onClick={() => navigate(`/recruitments/${id}/edit`)}
+    >
       {title}
     </Box>
   );
