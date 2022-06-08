41ce4148f9f1a039569e7a4bdcad77b7837f15bf
Naohiro Kosuda <kosnago0428@gmail.com>
2022-06-08 13:07:06 +0900

    UIの修正　

--- src/components/uiGroup/AppliedRecruitments/OtherContents.tsx
+++ src/components/uiGroup/AppliedRecruitments/OtherContents.tsx
@@ -1,14 +1,16 @@
-import { Box } from '@chakra-ui/react';
+import { Avatar, Box, Link } from '@chakra-ui/react';
 import { format } from 'date-fns';
 import { ja } from 'date-fns/locale';
 import { FC, memo } from 'react';
 
 type Props = {
+  avatar: string;
+  name: string;
   appliedAt: string;
 };
 
-export const AppliedAt: FC<Props> = memo((props) => {
-  const { appliedAt } = props;
+export const OtherContents: FC<Props> = memo((props) => {
+  const { appliedAt, avatar, name } = props;
   return (
     <Box
       fontFamily="ヒラギノ角ゴシック"
@@ -18,6 +20,11 @@ export const AppliedAt: FC<Props> = memo((props) => {
       display="flex"
       alignItems="center"
     >
+      <Link display="flex" alignItems="center">
+        <Avatar w={5} h={5} mr={1.5} src={avatar} />
+        <Box>{name}</Box>
+      </Link>
+      <Box mx={0.5}>・</Box>
       応募日時{' '}
       {format(new Date(appliedAt), 'yyyy年MM月dd日(E) HH:dd', { locale: ja })}
     </Box>
