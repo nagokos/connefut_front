a56bfbd3e9df46f344564e651b83c2906f00aacf
Naohiro Kosuda <kosnago0428@gmail.com>
2022-06-08 12:49:04 +0900

    コンポーネントを追加

--- src/components/uiGroup/DashboardButtons/index.tsx
+++ src/components/uiGroup/DashboardButtons/index.tsx
@@ -1,4 +1,5 @@
 import { FC, memo } from 'react';
+import { Applied } from './Applied';
 import { Recruitments } from './Recruitments';
 import { Stocks } from './Stocks';
 
@@ -6,6 +7,7 @@ export const DashboardButtons: FC = memo(() => {
   return (
     <>
       <Recruitments />
+      <Applied />
       <Stocks />
     </>
   );
