d30012a0a61e10ef74f6968fdd3d228f9512248f
Naohiro Kosuda <kosnago0428@gmail.com>
2022-06-08 12:45:20 +0900

    UIの修正

--- src/components/uiGroup/Header/index.tsx
+++ src/components/uiGroup/Header/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Divider, HStack, Spacer } from '@chakra-ui/react';
 import { FC, memo } from 'react';
+import { useLocation } from 'react-router-dom';
 import { useGetCurrentUserQuery } from '../../../generated/graphql';
 import { AppIn } from './AppIn';
 import { LoggedIn } from './LoggedIn';
@@ -8,6 +9,7 @@ import { Search } from './Search';
 import { Title } from './Title';
 
 export const Header: FC = memo(() => {
+  const location = useLocation();
   const [usrData] = useGetCurrentUserQuery({
     requestPolicy: 'network-only',
   });
@@ -24,7 +26,9 @@ export const Header: FC = memo(() => {
             {currentUser ? <LoggedIn user={currentUser} /> : <AppIn />}
           </HStack>
         </Box>
-        <Divider borderColor="#ebf2f2" />
+        {!location.pathname.includes('/messages') && (
+          <Divider borderColor="#ebf2f2" />
+        )}
       </header>
     </>
   );
