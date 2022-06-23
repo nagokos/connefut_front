135e8a74ccd827466124d2bdd05bfa353c0e5ee1
Naohiro Kosuda <kosnago0428@gmail.com>
2022-06-23 16:54:13 +0900

    /messagesでなければ遷移させる

--- src/components/uiGroup/Header/LoggedIn/Message.tsx
+++ src/components/uiGroup/Header/LoggedIn/Message.tsx
@@ -1,11 +1,12 @@
 import { FC, memo } from 'react';
 import { useBoolean } from '@chakra-ui/react';
 import { TbMessageCircle2 } from 'react-icons/tb';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export const Message: FC = memo(() => {
   const [flag, setFlag] = useBoolean();
   const navigate = useNavigate();
+  const location = useLocation();
 
   return (
     <TbMessageCircle2
@@ -14,7 +15,11 @@ export const Message: FC = memo(() => {
       fontSize={22}
       cursor="pointer"
       color={flag ? '#000000' : '#78909c'}
-      onClick={() => navigate('/messages')}
+      onClick={() => {
+        if (!location.pathname.includes('messages')) {
+          navigate('/messages');
+        }
+      }}
     />
   );
 });
