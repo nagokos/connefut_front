d8c67c48f27fdaf3a8628d5fed26f84d501d4b80
Naohiro Kosuda <kosnago0428@gmail.com>
2022-07-01 18:56:04 +0900

    relayを使用できるように修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import relay from 'vite-plugin-relay';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: true,
   },
-  plugins: [react()],
+  plugins: [react(), relay],
 });
