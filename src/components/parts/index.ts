a1e407beec8512cfbe4a1cf00d4b134a29a9b679
Naohiro Kosuda <kosnago0428@gmail.com>
2022-07-04 19:52:35 +0900

    add components

--- src/components/parts/index.ts
+++ src/components/parts/index.ts
@@ -1,1 +1,4 @@
 export { RecruitmentCard } from './RecruitmentCard';
+export { SearchInput } from './SearchInput';
+export { SearchSelect } from './SearchSelect';
+export { SearchTag } from './SearchTag';
