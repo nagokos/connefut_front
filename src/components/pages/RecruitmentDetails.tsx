ef3ea3520d611112d58d1c39f23036d555a9c742
Naohiro Kosuda <kosnago0428@gmail.com>
2022-07-07 14:49:44 +0900

    不要なファイルを削除

--- src/components/pages/RecruitmentDetails.tsx
+++
@@ -1,7 +0,0 @@
-import { FC, memo } from 'react';
-
-import { RecruitmentDetailsView } from '../views';
-
-export const RecruitmentDetails: FC = memo(() => {
-  return <RecruitmentDetailsView />;
-});
