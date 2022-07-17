4944531ef806482fb10b7d495bda9a840b308238
Naohiro Kosuda <kosnago0428@gmail.com>
2022-07-17 19:09:45 +0900

    不要なコードを削除

--- src/components/uiGroup/index.ts
+++ src/components/uiGroup/index.ts
@@ -1,4 +1,3 @@
-export { RecruitmentDetailActions } from './RecruitmentDetailActions';
 export { RecruitmentDetailUser } from './RecruitmentDetailUser';
 export { StockRecruitment } from './StockRecruitment';
 export { AppliedRecruitments } from './AppliedRecruitments';
